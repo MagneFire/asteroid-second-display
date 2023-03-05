62c09e3ef86bf6d97c996c260130f49a60f0c710
Darrel Griët <dgriet@gmail.com>
2023-03-06 00:11:40 +0100

    wip: catfish: Add timesync, timepiece and stepcounter functions.
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

--- src/backend_catfish.cpp
+++ src/backend_catfish.cpp
@@ -2,10 +2,124 @@
 
 #include <QDebug>
 
+#include <iostream>
+#include <dlfcn.h>
+#include <hybris/common/dlfcn.h>
+#include <hybris/properties/properties.h>
+#include <MGConfItem>
+
 using namespace AsteroidOS::SecondDisplayDaemon;
 
+CatfishBackend::CatfishBackend()
+{
+    mcutool_handle = OpenLibrary();
+    LoadSymbols();
+}
+
+CatfishBackend::~CatfishBackend()
+{
+    CloseLibrary(mcutool_handle);
+}
+
+void* CatfishBackend::OpenLibrary()
+{
+    auto lib_mcutool = hybris_dlopen("libmcutool.so", RTLD_LAZY);
+    if (!lib_mcutool) {
+        std::cerr << "Unable to load libmcutool.so" << std::endl;
+        return nullptr;
+    }
+    return lib_mcutool;
+}
+
+void* CatfishBackend::LoadSymbol(const char* symbol_string)
+{
+    void* symbol = hybris_dlsym(mcutool_handle, symbol_string);
+    if (!symbol) {
+        std::cerr << "Unable to get symbol " << symbol_string << std::endl;
+        return nullptr;
+    }
+    return symbol;
+}
+
+void CatfishBackend::LoadSymbols()
+{
+    if (!mcutool_handle) return;
+
+    nativeFunctions.autoLowPowerScreen = (int (*)(int32_t, int32_t, int32_t))LoadSymbol(
+        "Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeAutoLowPowerScreen");
+    nativeFunctions.bandMode = (int (*)())LoadSymbol("Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeBandMode");
+    nativeFunctions.cutOffScreen =
+        (int (*)())LoadSymbol("Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeCutOffScreen");
+    nativeFunctions.enableHeartRate = (int (*)(int32_t, int32_t, int32_t))LoadSymbol(
+        "Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeEnableHeartRate");
+    nativeFunctions.enableLowPowerScreen = (int (*)(int32_t, int32_t, int32_t))LoadSymbol(
+        "Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeEnableLowPowerScreen");
+    nativeFunctions.enableMotion = (int (*)(int32_t, int32_t, int32_t))LoadSymbol(
+        "Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeEnableMotion");
+    nativeFunctions.enableStepCounter = (int (*)(int32_t, int32_t, int32_t))LoadSymbol(
+        "Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeEnableStepCounter");
+    nativeFunctions.getBandModeData =
+        (int (*)())LoadSymbol("Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeGetBandModeData");
+    nativeFunctions.getDataVersion =
+        (int (*)())LoadSymbol("Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeGetDataVersion");
+    nativeFunctions.syncSteps = (int (*)(int32_t, int32_t, int32_t))LoadSymbol(
+        "Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeSyncSteps");
+    nativeFunctions.syncTime = (int (*)())LoadSymbol("Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeSyncTime");
+    nativeFunctions.updateFitnessState =
+        (int (*)(int32_t, int32_t, int32_t, int32_t, int32_t, int64_t, int32_t, float, int32_t, int32_t,
+                 uint8_t))LoadSymbol("Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeUpdateFitnessState");
+    nativeFunctions.wipeBandModeData =
+        (int (*)())LoadSymbol("Java_com_mobvoi_ticwear_mcuservice_CoreService_nativeWipeBandModeData");
+}
+
+int CatfishBackend::CloseLibrary(void* lib_mcutool)
+{
+    if (hybris_dlclose(lib_mcutool)) {
+        std::cerr << "Failed to safely close the library" << std::endl;
+        return -1;
+    }
+    return 0;
+}
 int CatfishBackend::SynchronizeTime()
 {
-    qDebug() << "catfish: Syncing time";
+    auto use12h = new MGConfItem("/org/asteroidos/settings/use-12h-format");
+
+    if (use12h->value(false).toBool()) {
+        property_set("persist.sys.time_12_24", "12");
+    } else {
+        property_set("persist.sys.time_12_24", "24");
+    }
+
+    return nativeFunctions.syncTime();
+}
+
+int CatfishBackend::EnterTimepieceMode()
+{
+    int res;
+    res = nativeFunctions.autoLowPowerScreen(0, 0, true);
+    if (res) {
+        std::cerr << "ERROR autoLowPowerScreen: " << res << std::endl;
+    }
+    res = nativeFunctions.autoLowPowerScreen(0, 0, false);
+    if (res) {
+        std::cerr << "ERROR autoLowPowerScreen: " << res << std::endl;
+    }
+    res = nativeFunctions.cutOffScreen();
+    if (res) {
+        std::cerr << "ERROR cutOffScreen: " << res << std::endl;
+    }
+    res = nativeFunctions.wipeBandModeData();
+    if (res) {
+        std::cerr << "ERROR wipeBandModeData: " << res << std::endl;
+    }
+    res = nativeFunctions.bandMode();
+    if (res) {
+        std::cerr << "ERROR bandMode: " << res << std::endl;
+    }
     return 1;
+}
+
+int CatfishBackend::SetStepCounter(bool enable)
+{
+    return nativeFunctions.enableStepCounter(0, 0, enable);
 }
--- src/backend_catfish.h
+++ src/backend_catfish.h
@@ -8,9 +8,21 @@ namespace AsteroidOS::SecondDisplayDaemon
 class CatfishBackend : public Backend
 {
    public:
-    CatfishBackend(){};
-    virtual ~CatfishBackend(){};
+    CatfishBackend();
+    virtual ~CatfishBackend();
+    virtual bool HasSecondDisplay() { return true; };
     virtual int SynchronizeTime();
+    virtual bool HasTimepieceMode() { return true; }
+    virtual int EnterTimepieceMode();
+    virtual bool HasStepCounter() { return true; }
+    virtual int SetStepCounter(bool enable);
+
+   private:
+    void* OpenLibrary();
+    void LoadSymbols();
+    void* LoadSymbol(const char* symbol_string);
+    int CloseLibrary(void* lib_mcutool);
+    void* mcutool_handle;
 };
 };      // namespace AsteroidOS::SecondDisplayDaemon
 #endif  // BACKEND_CATFISH_H
