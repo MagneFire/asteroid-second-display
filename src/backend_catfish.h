dc6c8d1959ea15cc5b40b79a9f96e950adfe1d92
Darrel Griët <dgriet@gmail.com>
2023-03-08 21:35:22 +0100

    wip: catfish: timepiece.
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

--- src/backend_catfish.h
+++ src/backend_catfish.h
@@ -23,6 +23,25 @@ class CatfishBackend : public Backend
     void* LoadSymbol(const char* symbol_string);
     int CloseLibrary(void* lib_mcutool);
     void* mcutool_handle;
+    typedef struct nativeFunctions_t {
+        int (*autoLowPowerScreen)(int32_t, int32_t, int32_t enable);
+        int (*bandMode)();
+        int (*cutOffScreen)();
+        int (*enableHeartRate)(int32_t, int32_t, int32_t enable);
+        int (*enableLowPowerScreen)(int32_t, int32_t, int32_t enable);
+        int (*enableMotion)(int32_t, int32_t, int32_t enable);
+        int (*enableStepCounter)(int32_t, int32_t, int32_t enable);
+        int (*getBandModeData)();
+        int (*getDataVersion)();
+        int (*syncSteps)(int32_t, int32_t, int32_t steps);
+        int (*syncTime)();
+        int (*updateFitnessState)(int32_t, int32_t, int32_t type, int32_t state, int32_t heartRate, int64_t duration,
+                                  int32_t calorie, float distance, int32_t speed, int32_t gpsStatus,
+                                  uint8_t isKilometer);
+        int (*wipeBandModeData)();
+    } nativeFunctions_t;
+
+    nativeFunctions_t nativeFunctions;
 };
 };      // namespace AsteroidOS::SecondDisplayDaemon
 #endif  // BACKEND_CATFISH_H
